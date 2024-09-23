ddf2ae0d231a9fa869f2ea638ffade7b8acca8b7
LikhitaChilla <163547908+LikhitaChilla@users.noreply.github.com>
2024-09-23 19:56:43 +0530

    Update new_apps.py

--- new_apps.py
+++ new_apps.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-import xgboost as xgb
 import pickle
 import regex
 import gzip
